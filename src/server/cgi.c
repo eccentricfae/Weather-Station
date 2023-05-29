e7879751d65e1080d7f5298fa835378599bf9c32
faeisanxious <adrianzabax@gmail.com>
2023-05-29 15:25:50 +0200

    Added prototype for debug page

--- src/server/cgi.c
+++ src/server/cgi.c
@@ -8,10 +8,17 @@ const char * home_handler(int index,
     return "/index.shtml";
 }
 
+const char * debug_handler(int index, 
+                           int num_params, 
+                           char * params[], 
+                           char * values[])
+{
+    return "/debug.shtml";
+}
 
 tCGI const cgi_handlers[] = {
-    { "/home", home_handler } // index 0;  <ip-addr>/home; also works <ip-addr> (nothing)
-    // IDEA Create debug page?
+    { "/home",  home_handler }, // index 0;  <ip-addr>/home; also works <ip-addr> (nothing)
+    { "/debug", debug_handler } // index 1; <ip-addr>/debug
 };
 
 
